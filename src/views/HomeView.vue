d8e32a94b07b372da316056881ef67b206b92247
Victor Chandra <victorchandra0911@gmail.com>
2022-06-16 16:26:45 +0700

    convert html home

---
+++ src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <HeaderShayna></HeaderShayna>
+    <HeroShayna></HeroShayna>
+    <WomenBannerShayna></WomenBannerShayna>
+    <InstaShayna></InstaShayna>
+    <PartnerLogoShayna></PartnerLogoShayna>
+    <FooterShayna></FooterShayna>
+  </div>
+</template>
+
+<script>
+import HeaderShayna from '../components/HeaderShayna.vue';
+import HeroShayna from '../components/HeroShayna.vue';
+import WomenBannerShayna from '../components/WomenBannerShayna.vue';
+import InstaShayna from '../components/InstaShayna.vue';
+import PartnerLogoShayna from '../components/PartnerLogoShayna.vue';
+import FooterShayna from '../components/FooterShayna.vue';
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    WomenBannerShayna,
+    InstaShayna,
+    PartnerLogoShayna,
+    FooterShayna
+}
+}
+</script>
